960ea3e5e865c412bfafc35f6280fd1ea4ff79c7
T3lo5 <anderson_wsf1@hotmail.com>
2021-11-09 17:30:50 -0300

    Foram feitos os exercicios para a fixação do VSCode

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,49 @@
+/* Exercicio 1 
+Será impresso no console após o primeiro console.log o valor de b como 10, já no segundo console.log será impresso o valor de a como 10 e o de b como 5
+*/
+/* Exercicio 2
+Serã impresso em console.log os valores de a, b e c, sendo estes valores, a = 10, b = 10 e c = 20
+*/
+/* Exercicio 3
+melhorando as váriaveis elas podem ficar 
+let horasDiarias 
+let ganhosDiarios
+Por fim o alert resultaria em um alerta com o valor de ganhosDiarios divido pelas horasDiarias assim dando o valor recebido por horás trabalhadas.
+*/
+/* /_Exercicio 4_/ */
+/* const name = prompt("Qual o seu nome?")
+let age = prompt("Qual a sua idade?")
+console.log (typeof name, typeof age) */
+/* Os tipos foram ambos string porque idade ẽ uma string já que não esta fixado que sõ podem ser nũmeros, jã o nome acaba sendo uma string porque todo nome é composto por letras
+*/
+/* /_Exercicio 5_/ */
+/* const name = prompt("Qual o seu nome?")
+let age = prompt("Qual a sua idade?")
+console.log (typeof name, typeof age) */
+/* Os tipos foram ambos string porque idade ẽ uma string já que não esta fixado que sõ podem ser nũmeros, jã o nome acaba sendo uma string porque todo nome é composto por letras
+*/
+/* /_Exercicio 6_/ */
+/* const name = prompt("Qual o seu nome?")
+let age = prompt("Qual a sua idade?")
+console.log ("Olã " +name, "você tem " +age + " anos")
+ */
+/* /_Exercicio 7_/ */
+/* let camisa = prompt("Você estã usando camisa?")
+let calça = prompt("Você está usando calça?")
+let sapato = prompt("Você está usando sapato?")
+let videogame = prompt("Você está jogando video game?")
+let carro = prompt("Você tem carro?")
+console.log ("Você está usando camisa? " + camisa)
+console.log ("Você está usando calça? " + calça)
+console.log ("Você está usando sapato? " + sapato)
+console.log ("Você está jogando video game? " + videogame)
+console.log ("Você tem carro? " + carro) */
+/* /_Exercicio 8_/ */
+let a = 15
+let b = 30
+let c = a 
+c = a 
+a = b 
+b = c
+console.log ("O novo valor de a ẽ = " + a) //novo valor de a é 30
+console.log ("O novo valor de b ẽ = " + b) //novo valor de b é 15
