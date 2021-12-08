5f76584ecb8744e1a8fa9bf75f9583f83359a9f2
T3lo5 <anderson_wsf1@hotmail.com>
2021-12-08 17:49:23 -0300

    Exercicio 13 b funcionando corretamente

--- modulo1/semana 3/lista-revisao-js/exercicios.js
+++ modulo1/semana 3/lista-revisao-js/exercicios.js
@@ -167,11 +167,19 @@ function retornaPessoasAutorizadas(pessoas) {
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+    var pessoasNaoAutorizadas = [];
+    for (var i = 0; i < pessoas.length; i++) {
+        if (pessoas[i].idade <= 14 || pessoas[i].idade > 60 || pessoas[i].altura < 1.5) {
+            pessoasNaoAutorizadas.push(pessoas[i]);
+        }
+    }
+    return pessoasNaoAutorizadas;
+
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
+    
 
 }
 
