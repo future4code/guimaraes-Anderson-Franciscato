ed12c75f9fb4613fde34ea99dccd8d320f102da2
T3lo5 <anderson_wsf1@hotmail.com>
2022-01-25 18:54:03 -0300

    Labetube com react

---
+++ modulo 3/jsx-labetube/labetube/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const titulo = "Titulo do video";
+
+  function reproduzVideo() {
+        alert("O vídeo está sendo reproduzido")
+    }
+
+return (
+  <div className="tela-inteira">
+            <h1>Lab Tube</h1>
+            <input type="text" placeholder="Busca" id="campoDeBusca"/>
+        <main>
+            <nav className="menu-vertical">
+                <ul>
+                    <li className="botoes-meunu-vertical">Início</li>
+                    <li className="botoes-meunu-vertical">Em alta</li>
+                    <li className="botoes-meunu-vertical">Inscrições</li>
+                    <hr/>
+                    <li className="botoes-meunu-vertical">Originais</li>
+                    <li className="botoes-meunu-vertical">Histórico</li>
+                </ul>
+            </nav>
+
+            <section className="painel-de-videos">
+                <div className="box-pagina-principal media1">
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=1 " alt=""/>
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media2" >
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=2 " alt=""/>
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media3" >
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=3" alt=""/>
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media4" >
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=4 " alt=""/>
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media5" >
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=5 " alt=""/>
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media6" >
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=6 " alt=""/>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div className="box-pagina-principal media7" >
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=7 " alt=""/>
+                    <h4>{titulo}</h4>
+                </div>
+                <div className="box-pagina-principal media8" >
+                    <img onClick={reproduzVideo} src="https://picsum.photos/400/400?a=8 " alt=""/>
+                    <h4>{titulo}</h4>
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <h4>Oi! Eu moro no footer!</h4>
+        </footer>
+    </div>
+)
+}
+
+export default App;
